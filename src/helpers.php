6561d03c72fb390415874b392363c62028536abc
bradstinson <brad@bradstinson.net>
2013-05-29 16:30:08 -0400

    Removed ENVIRONMENT test

--- src/helpers.php
+++ src/helpers.php
@@ -1,10 +1,6 @@
 <?php 
 
 function debug($data = NULL, $benchmark = FALSE) {
-    if (ENVIRONMENT === 'production')
-    {
-        return null;
-    }
 	
     list($call) = debug_backtrace();
     echo '<style>body, div {padding:0;margin:0;background:#fff;direction:ltr;}</style><div style="border-bottom:solid 1px #D8D8D8;background:#f1f1f1;color:#111;position:fixed;top:0px;left:0px;width:100%;padding:10px 20px;font-size:11px;font-family:arial,monospace;font-weight:normal;line-height:18px;">';
